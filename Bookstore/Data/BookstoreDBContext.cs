980e9e2f76d01f378b197dcb3236d684efc60ccf
loiy <lloiy9310@gmail.com>
2024-09-30 17:13:23 +0300

    Initial commit

---
+++ Bookstore/Data/BookstoreDBContext.cs
@@ -0,0 +1,43 @@
+﻿namespace Bookstore.Data
+{
+    using Bookstore.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class BookstoreDBContext : DbContext
+    {
+        public BookstoreDBContext(DbContextOptions<BookstoreDBContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasData(
+                new Book
+                {
+                    Id = 1,
+                    Title = "The Great Gatsby",
+                    Author = "F. Scott Fitzgerald",
+                    Price = 10.99m,
+                    Genre = "Classic"
+                },
+                new Book
+                {
+                    Id = 2,
+                    Title = "To Kill a Mockingbird",
+                    Author = "Harper Lee",
+                    Price = 7.99m,
+                    Genre = "Fiction"
+                },
+                new Book
+                {
+                    Id = 3,
+                    Title = "1984",
+                    Author = "George Orwell",
+                    Price = 8.99m,
+                    Genre = "Dystopian"
+                }
+            );
+        }
+    }
+
+}
---
+++ Bookstore/Migrations/20240929161311_init.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bookstore.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Genre", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, "F. Scott Fitzgerald", "Classic", 10.99m, "The Great Gatsby" },
+                    { 2, "Harper Lee", "Fiction", 7.99m, "To Kill a Mockingbird" },
+                    { 3, "George Orwell", "Dystopian", 8.99m, "1984" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
